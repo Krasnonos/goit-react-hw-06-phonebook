fc5b3a6f2410acd9eeba2917705b1de1a1ead1a8
Krasnonos <andreykrasnonos@icloud.com>
2022-08-10 16:01:06 +0300

    start persistore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
 import { ToastContainer } from 'react-toastify';
 import { App } from 'components/App';
 import { store } from './redux/redux';
@@ -10,8 +11,10 @@ import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
       <App />
       <ToastContainer />
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
--- src/redux/redux.js
+++ src/redux/redux.js
@@ -1,8 +1,17 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+
+// const persistConfig = {
+//   key: 'contacts',
+//   storage,
+// };
+
+// const persistedReducer = persistReducer(persistConfig, contactsSlice.reducer);
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: [{ id: '1', name: 'vova', number: '09965698983' }],
+  initialState: [],
   reducers: {
     addContact: (state, action) => {
       state.push(action.payload);
@@ -29,6 +38,8 @@ export const store = configureStore({
     filter: filterSlice.reducer,
   },
 });
+
+// export const  persistor = persistStore(store)
 
 export const { addContact, delateContact } = contactsSlice.actions;
 export const { changeFilter } = filterSlice.actions;
